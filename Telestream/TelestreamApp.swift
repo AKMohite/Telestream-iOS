995e4ba1a17da657861dbcae52a15876d6bec3a2
Ashish Mohite <ashishmohite1996@gmail.com>
2025-01-23 15:40:01 +0530

    Initial commit

---
+++ Telestream/TelestreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelestreamApp.swift
+//  Telestream
+//
+//  Created by Ashish Mohite on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TelestreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelestreamTests/TelestreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  TelestreamTests.swift
+//  TelestreamTests
+//
+//  Created by Ashish Mohite on 23/01/25.
+//
+
+import Testing
+@testable import Telestream
+
+struct TelestreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
