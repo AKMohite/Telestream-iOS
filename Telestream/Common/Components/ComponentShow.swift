1ef596e24cfb61d719b4e6f9a7e9b421647394bf
Ashish Mohite <ashishmohite1996@gmail.com>
2025-02-22 15:06:42 +0530

    Load image from url

--- Telestream/Common/Components/ComponentShow.swift
+++ Telestream/Common/Components/ComponentShow.swift
@@ -12,19 +12,11 @@ struct ComponentShow: View {
   let show: Show
 
     var body: some View {
-//      ZStack {
-//        RoundedRectangle(cornerRadius: 8)
-//          .fill(.gray)
-//        Image(systemName: "folder")
-//          .frame(width: .infinity, height: .infinity)
-//        Text(show.title)
-//          .font(.title3)
-//          .lineLimit(2)
-//      }.frame(width: 250, height: 300)
-//      TODO: load image from url
-      Image(systemName: "folder")
+      AsyncImage(url: URL(string: show.image)) { image in
+        image.image?.resizable()
+          .scaledToFill()
+      }
         .frame(width: 200, height: 250)
-        .scaledToFill()
         .background(.gray.opacity(0.25))
         .overlay {
           TitleOverlay(title: show.title)
